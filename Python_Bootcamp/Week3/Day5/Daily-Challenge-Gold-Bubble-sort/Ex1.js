f9fc5f08269f1beadd982a2e6d2dbd70c6e33b75
lavalliu <146725126+lavalliu@users.noreply.github.com>
2023-11-15 18:28:43 +0400

    finish  Recreate Landing Page &  Recreate Land Pg2

--- Python_Bootcamp/Week3/Day5/Daily-Challenge-Gold-Bubble-sort/Ex1.js
+++ Python_Bootcamp/Week3/Day5/Daily-Challenge-Gold-Bubble-sort/Ex1.js
@@ -5,105 +5,128 @@ const prompt = require("prompt-sync")();
 
 //bubble sort
 
-const numbers = [5,0,9,1,7,4,2,6,3,8];
+// const numbers = [5,0,9,1,7,4,2,6,3,8];
 
-let text=numbers.toString();
+// let text=numbers.toString();
 
-console.log(text);
+// console.log(text);
 
-let text1=numbers.join();
+// let text1=numbers.join();
 
-console.log(text1);
+// console.log(text1);
 
-let x=0;
-let y=0;
+// let x=0;
+// let y=0;
 
-for (let i = 0; i < 10; i++) {
-  console.log("i"+i);
-  let a=numbers[i]
-  let b=numbers[i+1]
-  switch (a>b) {
-    case true:
-        console.log("a"+a)
-        console.log("b"+b)
-        numbers.splice(i+1,1,a);
-        console.log(numbers);
-        numbers.splice(i,1,b);
-        console.log(numbers);
-    default:
+// for (let i = 0; i < 10; i++) {
+//   console.log("i"+i);
+//   let a=numbers[i]
+//   let b=numbers[i+1]
+//   switch (a>b) {
+//     case true:
+//         console.log("a"+a)
+//         console.log("b"+b)
+//         numbers.splice(i+1,1,a);
+//         console.log(numbers);
+//         numbers.splice(i,1,b);
+//         console.log(numbers);
+//     default:
+//     }
+
+
+//   }
+//   if (i=9) {
+//     let i=1;
+//   }
+//   for (let i = 1; i < 10; i++) {
+//     console.log("i"+i);
+//     let a=numbers[i]
+//     let b=numbers[i+1]
+//     switch (a>b) {
+//       case true:
+//           console.log("a"+a)
+//           console.log("b"+b)
+//           numbers.splice(i+1,1,a);
+//           console.log(numbers);
+//           numbers.splice(i,1,b);
+//           console.log(numbers);
+//       default:
+//       }
+// }
+// if (i=8) {
+//   let i=1;
+// }
+// for (let i = 1; i < 10; i++) {
+//   console.log("i"+i);
+//   let a=numbers[i]
+//   let b=numbers[i+1]
+//   switch (a>b) {
+//     case true:
+//         console.log("a"+a)
+//         console.log("b"+b)
+//         numbers.splice(i+1,1,a);
+//         console.log(numbers);
+//         numbers.splice(i,1,b);
+//         console.log(numbers);
+//     default:
+//     }
+//   }
+//   if (i=7) {
+//     let i=1;
+//   }
+//   for (let i = 1; i < 10; i++) {
+//     console.log("i"+i);
+//     let a=numbers[i]
+//     let b=numbers[i+1]
+//     switch (a>b) {
+//       case true:
+//           console.log("a"+a)
+//           console.log("b"+b)
+//           numbers.splice(i+1,1,a);
+//           console.log(numbers);
+//           numbers.splice(i,1,b);
+//           console.log(numbers);
+//       default:
+//       }
+//     }
+//     if (i=6) {
+//       let i=1;
+//     }
+//     for (let i = 1; i < 10; i++) {
+//       console.log("i"+i);
+//       let a=numbers[i]
+//       let b=numbers[i+1]
+//       switch (a>b) {
+//         case true:
+//             console.log("a"+a)
+//             console.log("b"+b)
+//             numbers.splice(i+1,1,a);
+//             console.log(numbers);
+//             numbers.splice(i,1,b);
+//             console.log(numbers);
+//         default:
+//         }
+//       }
+
+
+      //from nirmal
+var nums=[5,0,9,1,7,4,2,6,3,8];
+j=0;
+do  {
+swapped=false;
+
+  swapped=false;
+  for (let i=0; i<nums.lenght;i++){
+    if (nums[i]>nums[i+1]){
+      temp=nums[i];
+      nums[i]=nums[i+1];
+      nums[i+1]=temp;
+      swapped=true;
     }
+  }
+    j=j+1;
+    console.log('Pass '+j.tostring()+' \t'+nums.tostring());
+} while (swapped==true);
 
+console.log(nums);
 
-  }
-  if (i=9) {
-    let i=1;
-  }
-  for (let i = 1; i < 10; i++) {
-    console.log("i"+i);
-    let a=numbers[i]
-    let b=numbers[i+1]
-    switch (a>b) {
-      case true:
-          console.log("a"+a)
-          console.log("b"+b)
-          numbers.splice(i+1,1,a);
-          console.log(numbers);
-          numbers.splice(i,1,b);
-          console.log(numbers);
-      default:
-      }
-}
-if (i=8) {
-  let i=1;
-}
-for (let i = 1; i < 10; i++) {
-  console.log("i"+i);
-  let a=numbers[i]
-  let b=numbers[i+1]
-  switch (a>b) {
-    case true:
-        console.log("a"+a)
-        console.log("b"+b)
-        numbers.splice(i+1,1,a);
-        console.log(numbers);
-        numbers.splice(i,1,b);
-        console.log(numbers);
-    default:
-    }
-  }
-  if (i=7) {
-    let i=1;
-  }
-  for (let i = 1; i < 10; i++) {
-    console.log("i"+i);
-    let a=numbers[i]
-    let b=numbers[i+1]
-    switch (a>b) {
-      case true:
-          console.log("a"+a)
-          console.log("b"+b)
-          numbers.splice(i+1,1,a);
-          console.log(numbers);
-          numbers.splice(i,1,b);
-          console.log(numbers);
-      default:
-      }
-    }
-    if (i=6) {
-      let i=1;
-    }
-    for (let i = 1; i < 10; i++) {
-      console.log("i"+i);
-      let a=numbers[i]
-      let b=numbers[i+1]
-      switch (a>b) {
-        case true:
-            console.log("a"+a)
-            console.log("b"+b)
-            numbers.splice(i+1,1,a);
-            console.log(numbers);
-            numbers.splice(i,1,b);
-            console.log(numbers);
-        default:
-        }
-      }

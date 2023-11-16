fffb6b256372d1c40002376187d8faf99cf10554
lavalliu <146725126+lavalliu@users.noreply.github.com>
2023-11-16 14:37:51 +0400

    Completed daily Challenge NOT BAD

---
+++ Python_Bootcamp/Week4/Mini-Project-Hangman/HM.js
@@ -0,0 +1,10 @@
+secretword=prompt("Player 1, please enter a secret Word: ")
+let i=0
+let win=true;
+while (i<10 && true) {
+    letter=prompt("Player 2, try to guess the word by proposing a letter: ")
+    switch secretword.indexof(letter)!==-1 {
+
+    }
+
+    }
